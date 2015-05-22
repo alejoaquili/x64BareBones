95aac6806a70c21f368a36a7872076df4fbcf40d
RowDaBoat <RowDaBoat>
2015-05-22 15:07:25 -0300

    -kernel: Fix code and data sample modules loading addresses. This was causing errors when accessing the BSS. -module packer: replace LOG macro with printfs -sample code module: add bss and endOfBinary identifiers, add memset method and properly clean the BSS, add some test code to check correct initialization.

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -13,8 +13,8 @@ extern uint8_t endOfKernel;
 
 static const uint64_t PageSize = 0x1000;
 
-static void * const sampleCodeModuleAddress = (void*)0x40000;
-static void * const sampleDataModuleAddress = (void*)0x50000;
+static void * const sampleCodeModuleAddress = (void*)0x400000;
+static void * const sampleDataModuleAddress = (void*)0x500000;
 
 typedef int (*EntryPoint)();
 
